6c8db6ad6b99cb6dba0037c813e7333e69851740
arthurcordova <arthurnyhc@icloud.com>
2016-07-05 21:31:26 -0300

    Login screen created

--- app/src/main/java/br/com/artechapps/app/activity/SplashActivity.java
+++ app/src/main/java/br/com/artechapps/app/activity/SplashActivity.java
@@ -1,5 +1,6 @@
 package br.com.artechapps.app.activity;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -12,5 +13,11 @@ public class SplashActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
 
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        startActivity(new Intent(this, LoginActivity.class));
     }
 }
