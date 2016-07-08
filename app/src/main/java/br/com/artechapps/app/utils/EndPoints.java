109b018cc57d755672bcde3fcb1f2b4c952c51d0
arthurcordova <arthurnyhc@icloud.com>
2016-07-08 20:03:43 -0300

    Config endpoint ws

---
+++ app/src/main/java/br/com/artechapps/app/utils/EndPoints.java
@@ -0,0 +1,10 @@
+package br.com.artechapps.app.utils;
+
+/**
+ * Created by arthurcordova on 7/8/16.
+ */
+public class EndPoints extends Webservice{
+
+    public static final String LOGIN = SERVER.concat("/login");
+
+}
---
+++ app/src/main/java/br/com/artechapps/app/utils/Webservice.java
@@ -0,0 +1,11 @@
+package br.com.artechapps.app.utils;
+
+import br.com.artechapps.app.BuildConfig;
+
+/**
+ * Created by arthurcordova on 7/8/16.
+ */
+public abstract class Webservice {
+
+    protected static final String SERVER = BuildConfig.HOST.concat("/clinwebservice/servidor");
+}
