39b2727196f6bb3afd2569de8584096258cb0589
Arthur Cordova Stapassoli <arthur.stapassoli@gmail.com>
2016-07-19 23:35:31 -0300

    Cart details activity

--- app/src/main/java/br/com/artechapps/app/activity/CartActivity.java
+++ app/src/main/java/br/com/artechapps/app/activity/CartActivity.java
@@ -1,21 +1,16 @@
 package br.com.artechapps.app.activity;
 
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.DefaultItemAnimator;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
-import android.view.View;
 
 import java.util.ArrayList;
 
 import br.com.artechapps.app.R;
 import br.com.artechapps.app.adapter.RVAdapterCart;
-import br.com.artechapps.app.adapter.RVAdapterProduct;
-import br.com.artechapps.app.database.PersistenceProduct;
 import br.com.artechapps.app.database.PersistenceShop;
 import br.com.artechapps.app.model.Shop;
 
@@ -31,10 +26,9 @@ public class CartActivity extends AppCompatActivity {
         setContentView(R.layout.activity_cart);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
         mRvCart = (RecyclerView) findViewById(R.id.rv_cart_detail);
-
-
         mPersistence = new PersistenceShop(this);
         mList = mPersistence.getRecords();
 
@@ -44,8 +38,5 @@ public class CartActivity extends AppCompatActivity {
         mRvCart.setItemAnimator(new DefaultItemAnimator());
         mRvCart.setAdapter(mAdapter);
 
-
-
     }
-
 }
