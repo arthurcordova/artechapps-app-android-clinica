88ab607e0d38b6037bf53868b8e9804a48f0175f
Arthur Cordova Stapassoli <arthur.stapassoli@gmail.com>
2016-07-29 20:38:16 -0300

    Message detail layout

--- app/src/main/java/br/com/artechapps/app/activity/DetailMessageActivity.java
+++ app/src/main/java/br/com/artechapps/app/activity/DetailMessageActivity.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
+import android.widget.TextView;
 
 import br.com.artechapps.app.R;
 import br.com.artechapps.app.model.Message;
@@ -11,6 +12,10 @@ import br.com.artechapps.app.model.Message;
 public class DetailMessageActivity extends AppCompatActivity {
 
     private Message mModel;
+    private TextView tvMsgTitle;
+    private TextView tvMsgContent;
+    private TextView tvDateSent;
+    private TextView tvDateSee;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,10 +27,17 @@ public class DetailMessageActivity extends AppCompatActivity {
         mModel = (Message) getIntent().getSerializableExtra("model");
 
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
-        getSupportActionBar().setTitle("");
+        getSupportActionBar().setTitle("Mensagem");
 
+        tvMsgTitle = (TextView) findViewById(R.id.tv_msg_title);
+        tvMsgContent = (TextView) findViewById(R.id.tv_msg_content);
+        tvDateSent = (TextView) findViewById(R.id.tv_date_sent);
+        tvDateSee = (TextView) findViewById(R.id.tv_date_see);
 
-
+        tvMsgTitle.setText(mModel.getTitle());
+        tvMsgContent.setText(mModel.getMessage());
+        tvDateSent.setText("Enviada em " + mModel.getSentDate());
+        tvDateSee.setText(mModel.getSeeDate() != null ? "Visualizada em  " + mModel.getSeeDate() : "");
     }
 
     @Override
