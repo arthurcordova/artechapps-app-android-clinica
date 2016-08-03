de3091dbc4bfd2f4a95e2628ab2163aef350861d
Arthur Cordova Stapassoli <arthur.stapassoli@gmail.com>
2016-08-02 23:57:23 -0300

    Schedule choose product list

--- app/src/main/java/br/com/artechapps/app/activity/NewScheduleActivity.java
+++ app/src/main/java/br/com/artechapps/app/activity/NewScheduleActivity.java
@@ -2,14 +2,44 @@ package br.com.artechapps.app.activity;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.DefaultItemAnimator;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+
+import java.util.ArrayList;
 
 import br.com.artechapps.app.R;
+import br.com.artechapps.app.adapter.RVAdapterProductSchedule;
+import br.com.artechapps.app.database.PersistenceProduct;
+import br.com.artechapps.app.model.Product;
 
 public class NewScheduleActivity extends AppCompatActivity {
+
+    private RecyclerView mRvProduct;
+    private PersistenceProduct mPersistence;
+    private ArrayList<Product> mList;
+    private RVAdapterProductSchedule mRvAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_new_schedule);
+
+        mRvProduct = (RecyclerView) findViewById(R.id.rvProducts);
+
+//        new AsyncTaskProduct("Carregando produtos...",this,true, mRvProduct, ).execute(String.valueOf(BuildConfig.FILIAL));
+
+        mPersistence = new PersistenceProduct(this);
+        mList = mPersistence.getProduct();
+
+        mRvAdapter = new RVAdapterProductSchedule(mList, this);
+
+        mRvProduct.setLayoutManager(new LinearLayoutManager(this));
+        mRvProduct.setItemAnimator(new DefaultItemAnimator());
+        mRvProduct.setAdapter(mRvAdapter);
+
+        mPersistence.close();
+
+
     }
 }
---
+++ app/src/main/java/br/com/artechapps/app/adapter/RVAdapterProductSchedule.java
@@ -0,0 +1,92 @@
+package br.com.artechapps.app.adapter;
+
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+
+import br.com.artechapps.app.R;
+import br.com.artechapps.app.activity.NewScheduleActivity;
+import br.com.artechapps.app.model.Product;
+
+/**
+ * Created by acstapassoli on 07/12/2015.
+ */
+public class RVAdapterProductSchedule extends RecyclerView.Adapter<RVAdapterProductSchedule.ViewHolder> {
+
+    private ArrayList<Product> mItemsData;
+    private NewScheduleActivity mActivity;
+
+    public RVAdapterProductSchedule(ArrayList<Product> itemsData, NewScheduleActivity activity) {
+        mItemsData = itemsData;
+        mActivity = activity;
+    }
+
+    @Override
+    public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View itemLayoutView = LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.list_item_product_schedule, null);
+        ViewHolder viewHolder = new ViewHolder(itemLayoutView, mActivity);
+        return viewHolder;
+    }
+
+    // Replace the contents of a view (invoked by the layout manager)
+    @Override
+    public void onBindViewHolder(ViewHolder viewHolder, int position) {
+        Product model = mItemsData.get(position);
+        viewHolder.tvCode.setText(String.valueOf(model.getId()));
+
+        viewHolder.tvDescription.setText(model.getDescription());
+        viewHolder.tvAdd.setText("R$ " + model.formatValue(model.getValue()));
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return (mItemsData != null) ? mItemsData.size() : 0;
+    }
+
+    // inner class to hold a reference to each item of RecyclerView
+    public static class ViewHolder extends RecyclerView.ViewHolder {
+
+        TextView tvCode;
+        TextView tvDescription;
+        TextView tvValue;
+        TextView tvAdd;
+
+        NewScheduleActivity mActivity;
+
+        public ViewHolder(View itemLayoutView, final NewScheduleActivity activity) {
+            super(itemLayoutView);
+            mActivity = activity;
+            tvCode = (TextView) itemLayoutView.findViewById(R.id.code);
+            tvDescription = (TextView) itemLayoutView.findViewById(R.id.description);
+            tvValue = (TextView) itemLayoutView.findViewById(R.id.value);
+            tvAdd = (TextView)itemLayoutView.findViewById(R.id.add);
+
+//            tvAdd.setOnClickListener(new View.OnClickListener() {
+//                @Override
+//                public void onClick(View v) {
+//
+//                    Shop s = new Shop();
+//                    Product p = new Product();
+//                    p.setId(Long.parseLong(tvCode.getText().toString()));
+//                    s.setProduct(p);
+//
+//                    PersistenceShop persistenceShop = null;
+//                    try {
+//                        persistenceShop = new PersistenceShop(activity);
+//                        persistenceShop.save(s);
+//                        Toast.makeText(v.getContext(), "Produto adicionado ao carrinho.", Toast.LENGTH_SHORT).show();
+//
+//                    } finally {
+//                        persistenceShop.close();
+//                    }
+//                }
+//            });
+        }
+    }
+}
