17ba5f6b5ef7f7b01fe40d2b0d34d37b125e3b0b
arthurcordova <arthur.stapassoli@gmail.com>
2017-10-09 20:42:05 -0300

    Vibrate notification.

--- app/src/main/java/br/com/artechapps/app/fcm/BeautyFirebaseMessagingService.java
+++ app/src/main/java/br/com/artechapps/app/fcm/BeautyFirebaseMessagingService.java
@@ -4,6 +4,8 @@ import android.app.NotificationManager;
 import android.app.PendingIntent;
 import android.content.Context;
 import android.content.Intent;
+import android.media.RingtoneManager;
+import android.net.Uri;
 import android.support.v4.app.NotificationCompat;
 import android.util.Log;
 
@@ -36,6 +38,7 @@ public class BeautyFirebaseMessagingService extends FirebaseMessagingService {
         notificationBuilder.setSmallIcon(R.mipmap.ic_logo);
         notificationBuilder.setAutoCancel(true);
         notificationBuilder.setContentIntent(pendingIntent);
+        notificationBuilder.setVibrate(new long[] { 500, 500, 500, 500, 500});
 
         NotificationManager notificationManager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
         notificationManager.notify(0, notificationBuilder.build());
